96820027d517cc7d7b5e45a1a5988e888a5c3130
asier.pastor <asier.pastor@getronics.com>
2025-07-13 20:00:41 +0200

    Add logs to review production problem

--- src/main/java/com/asierpc/portfoliobackend/adapter/rest/AuthController.java
+++ src/main/java/com/asierpc/portfoliobackend/adapter/rest/AuthController.java
@@ -1,4 +1,8 @@
+
 package com.asierpc.portfoliobackend.adapter.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.asierpc.portfoliobackend.adapter.mapper.AuthMapper;
 import com.asierpc.portfoliobackend.api.AuthenticationApi;
@@ -19,24 +23,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController implements AuthenticationApi {
 
+  private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
   private final AuthServicePort authService;
   private final AuthMapper authMapper;
 
   @Override
   public ResponseEntity<AuthResponse> loginUser(LoginRequest loginRequest) {
-    LoginCommand command = authMapper.toLoginCommand(loginRequest);
-    String token = authService.login(command);
-    AuthResponse response = new AuthResponse();
-    response.setToken(token);
-    return ResponseEntity.ok(response);
+    log.info("[LOGIN] Petición recibida: {}", loginRequest);
+    try {
+      LoginCommand command = authMapper.toLoginCommand(loginRequest);
+      log.debug("[LOGIN] LoginCommand: {}", command);
+      String token = authService.login(command);
+      log.info("[LOGIN] Token generado correctamente");
+      AuthResponse response = new AuthResponse();
+      response.setToken(token);
+      return ResponseEntity.ok(response);
+    } catch (Exception e) {
+      log.error("[LOGIN] Error en loginUser: {}", e.getMessage(), e);
+      throw e;
+    }
   }
 
   @Override
   public ResponseEntity<AuthResponse> registerUser(RegisterRequest registerRequest) {
-    RegisterCommand command = authMapper.toRegisterCommand(registerRequest);
-    String token = authService.register(command);
-    AuthResponse response = new AuthResponse();
-    response.setToken(token);
-    return ResponseEntity.status(201).body(response);
+    log.info("[REGISTER] Petición recibida: {}", registerRequest);
+    try {
+      RegisterCommand command = authMapper.toRegisterCommand(registerRequest);
+      log.debug("[REGISTER] RegisterCommand: {}", command);
+      String token = authService.register(command);
+      log.info("[REGISTER] Token generado correctamente");
+      AuthResponse response = new AuthResponse();
+      response.setToken(token);
+      return ResponseEntity.status(201).body(response);
+    } catch (Exception e) {
+      log.error("[REGISTER] Error en registerUser: {}", e.getMessage(), e);
+      throw e;
+    }
   }
 }
